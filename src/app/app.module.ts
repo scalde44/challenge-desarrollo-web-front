ed616cb57f1e15baf80c71977edccc705af78151
Steven Calderon <scalderon1504@gmail.com>
2022-06-30 16:24:25 -0500

    Ajuste para variables de websocket

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WebsocketService } from './services/websocket.service';
+import { PruebaConnectComponent } from './components/prueba-connect/prueba-connect.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PruebaConnectComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [WebsocketService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
