a3065440fcc30e549f25d50b810b7d3fd38f579f
Steven Calderon <scalderon1504@gmail.com>
2022-07-01 09:23:52 -0500

    Se ajusta el app module y sus rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { PruebaConnectComponent } from './components/prueba-connect/prueba-connect.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PruebaConnectComponent,
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./feature/auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'cargame',
+    loadChildren: () =>
+      import('./feature/cargame/cargame.module').then((m) => m.CargameModule),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      preloadingStrategy: PreloadAllModules,
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { WebsocketService } from './services/websocket.service';
 import { PruebaConnectComponent } from './components/prueba-connect/prueba-connect.component';
+import { CoreModule } from './core/core.module';
+import { WebsocketService } from './prueba-ws/services/websocket.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PruebaConnectComponent
-  ],
+  declarations: [AppComponent, PruebaConnectComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CoreModule,
   ],
   providers: [WebsocketService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
