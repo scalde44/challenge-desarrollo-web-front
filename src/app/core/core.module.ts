ed13b6acb5c2989911df2cd4a3d018aa3e6bb3f5
Sebastian Pineda <63763483+sebascamilo12@users.noreply.github.com>
2022-07-01 11:06:52 -0500

    se actualizan los modulos

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MaterialModule } from '../material/material.module';
+
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+   
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule
   ]
 })
 export class CoreModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MaterialModule } from '../material/material.module';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ToolbarComponent,
+    NavbarComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    RouterModule
+    
+  ],
+  exports:[
+    ToolbarComponent,
+    NavbarComponent
   ]
 })
 export class SharedModule { }
