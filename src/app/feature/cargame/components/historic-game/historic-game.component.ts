136340d53c6a82c50215fa0dc6e5ecb7ec246f71
Sebastian Pineda <63763483+sebascamilo12@users.noreply.github.com>
2022-07-01 11:04:29 -0500

    Se crean componentes

---
+++ src/app/feature/cargame/components/historic-game/historic-game.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-historic-game',
+  templateUrl: './historic-game.component.html',
+  styleUrls: ['./historic-game.component.scss']
+})
+export class HistoricGameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/feature/cargame/components/landing-game/landing-game.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-game',
+  templateUrl: './landing-game.component.html',
+  styleUrls: ['./landing-game.component.scss']
+})
+export class LandingGameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
