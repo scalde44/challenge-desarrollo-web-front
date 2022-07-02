a2a50a62dd6fff3706756b7d3ec771997165f7a3
Steven Calderon <scalderon1504@gmail.com>
2022-07-02 16:22:19 -0500

    Prueba inicial del juego

--- src/app/feature/cargame/components/start-game/start-game.component.ts
+++ src/app/feature/cargame/components/start-game/start-game.component.ts
@@ -3,13 +3,36 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-start-game',
   templateUrl: './start-game.component.html',
-  styleUrls: ['./start-game.component.scss']
+  styleUrls: ['./start-game.component.scss'],
 })
 export class StartGameComponent implements OnInit {
+  carros: any[] = [
+    { id: 1, posicion: 0, color: 'rojo' },
+    { id: 2, posicion: 0, color: 'amarillo' },
+    { id: 3, posicion: 0, color: 'rojo' },
+  ];
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  updatePosition(idCar: number) {
+    const divCar = document.getElementById(this.getNombreIdCar(idCar));
+    const posicion = this.getPosicionCarroById(idCar);
+    divCar.style.transform = `translateX(${posicion}px)`;
   }
 
+  getNombreIdCar(idCar: number): string {
+    return 'carro' + idCar;
+  }
+
+  private getPosicionCarroById(carroId: number): number {
+    let index = this.carros.findIndex((obj) => obj.id == carroId);
+    this.carros[index].posicion = this.carros[index].posicion + 10;
+    return this.carros[index].posicion;
+  }
+
+  getColorCarroById(carroId: number): string {
+    let carro = this.carros.find((carro) => carro.id === carroId);
+    return `assets/images/carro-${carro.color}.png`;
+  }
 }
