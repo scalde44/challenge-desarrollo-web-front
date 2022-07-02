75615df416d4fa49dfe243ac8ceb024bda80540b
JosueArdila <42660657+JosueArdila@users.noreply.github.com>
2022-07-02 10:32:57 -0500

    Se crea componente login

--- src/app/feature/auth/components/login/login.component.ts
+++ src/app/feature/auth/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { MessageService } from '../../services/message.service';
+import { Constants } from 'src/app/shared/class/constants';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  email : string = '';
+  password : string = '';
+
+  constructor(private authService : AuthService, private messageService : MessageService) { }
 
   ngOnInit(): void {
+  }
+
+  login() {
+    if(this.email == ''){
+      
+      this.messageService.alert(Constants.WARNING_ICON, Constants.TITLE, Constants.EMAIL_BLANK);
+      return;
+    }
+
+    if(this.password == ''){
+      this.messageService.alert(Constants.WARNING_ICON, Constants.TITLE, Constants.PASSWORD_BLANK);
+      return;
+    }
+
+    this.authService.login(this.email, this.password);      
+  }
+
+  loginWithGoogle() {
+    this.authService.googleSignIn();    
   }
 
 }
