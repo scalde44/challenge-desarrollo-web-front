cde1b4c9468df0c4486fcb9b09a3d7072a9ea544
JosueArdila <42660657+JosueArdila@users.noreply.github.com>
2022-07-03 16:32:21 -0500

    Se crea el método para obtener el histórico de juegos

--- src/app/feature/cargame/services/juego.service.ts
+++ src/app/feature/cargame/services/juego.service.ts
@@ -1,10 +1,14 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
 import { Observable } from 'rxjs/internal/Observable';
 import { environment } from 'src/environments/environment';
 import { CrearJuegoCommand } from '../models/commands/crear-juego-command.model';
 import { IniciarJuegoCommand } from '../models/commands/iniciar-juego-command.model';
 import { JuegoCreado } from '../models/events/juego-creado.model';
+import { Historico } from '../models/objects/Historico.model';
+import { HistoricoTabla } from '../models/objects/historicotabla.model';
+import { Podio } from '../models/objects/podio.model';
 
 const API_ENDPOINT = environment.apiUrl;
 @Injectable()
@@ -35,5 +39,23 @@ export class JuegoService {
       }),
       responseType: 'text',
     });
+  }
+
+  public getPodio(juegoId : string): Observable<Podio> {
+    return this.httpClient.get<Podio>(`${API_ENDPOINT}/podio/${juegoId}`);
+  }
+
+  public getHistoricoGanadores() : Observable<HistoricoTabla[]> {
+    return this.httpClient.get<Historico[]>(`${API_ENDPOINT}/ganadores`).pipe(
+      map((historico: Historico[])=> historico.map((h)=>{
+        const historicoTabla: HistoricoTabla = {
+          aggregateRootId : h.aggregateRootId,
+          idGanador : h.ganador[0].jugadorId.uuid,
+          nombreGanador : h.ganador[0].nombre.value,
+          colorGanador : h.ganador[0].color.value
+        };
+        return historicoTabla;
+      }))
+    );  
   }
 }
