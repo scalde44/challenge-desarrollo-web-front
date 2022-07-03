53d6303f36292966238211b99af1e4aa3b942998
JosueArdila <42660657+JosueArdila@users.noreply.github.com>
2022-07-03 16:29:46 -0500

    Se implementa el componente del histórico de juegos

--- src/app/feature/cargame/components/historic-game/historic-game.component.ts
+++ src/app/feature/cargame/components/historic-game/historic-game.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Historico } from '../../models/objects/Historico.model';
+import { JuegoService } from '../../services/juego.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { HistoricoTabla } from '../../models/objects/historicotabla.model';
 
 @Component({
   selector: 'app-historic-game',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./historic-game.component.scss']
 })
 export class HistoricGameComponent implements OnInit {
+  
+ 
+  title: string = 'Historico de ganadores';
+  listaHistorico: HistoricoTabla[] = [];
+  displayedColumns: string[] = ['aggregateRootId', 'idGanador', 'nombreGanador', 'colorGanador'];
+  dataSource = new MatTableDataSource<HistoricoTabla>(this.listaHistorico);
 
-  constructor() { }
+  constructor(private juegoService : JuegoService) { }
 
   ngOnInit(): void {
+    this.getHistorico();   
   }
+
+  getHistorico() {
+    this.juegoService.getHistoricoGanadores().subscribe(
+      historico  => {
+        this.listaHistorico = historico;
+        this.dataSource.data = this.listaHistorico;
+        console.log(this.dataSource.data);
+      }
+    )
+  }
+
+
+
 
 }
